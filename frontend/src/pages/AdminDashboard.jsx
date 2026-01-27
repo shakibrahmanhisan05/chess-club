eed6f8a1807924e473a1dc2ee2098715b4154ea4
shakibrahmanhisan05 <shakibrahmanhisanpc@gmail.com>
2026-01-27 18:27:55 +0600

    temporary: silence exhaustive-deps warning in AdminDashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -54,7 +54,7 @@ export default function AdminDashboard() {
       navigate('/admin/login');
     }
   }, [isAuthenticated, isLoading, navigate]);
-
+// eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (token) {
       fetchAllData();
